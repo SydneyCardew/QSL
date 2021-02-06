6aac171c4f7fcbdd0fef26f8911f4ff3ad184b8d
Sydney Cardew <sydney.cardew@gmail.com>
2021-02-06 05:53:03 +0000

    Cleaned up code

--- QSL.py
+++ QSL.py
@@ -5,7 +5,7 @@ from datetime import date
 from random import seed
 from random import randint
 
-def QSLer(qpicklist,spicklist,lpicklist):
+def QSLer(qpicklist,spicklist,lpicklist): # Generates the QSL
     seed()
     qlist = []
     slist = []
@@ -30,17 +30,17 @@ def QSLer(qpicklist,spicklist,lpicklist):
                 llist.append(line.strip('\n\r'))
     qpicker = randint(0, len(qlist)-1)
     if qpicker == qpicklist[-1]: # this if avoids repeated Qs
-        qpicker += 1
+        qpicker += randint(2,6)
         if qpicker > len(qlist)-1:
             qpicker = 1
     spicker = randint(0, len(slist)-1)
     if spicker == spicklist[-1]: # this if avoids repeated Ss
-        spicker += 1
+        spicker += randint(2,6)
         if spicker > len(slist)-1:
             spicker = 1
     lpicker = randint(0, len(llist)-1)
     if lpicker == lpicklist[-1]: # this if avoids repeated Ls
-        lpicker += 1
+        lpicker += randint(2,6)
         if lpicker > len(llist)-1:
             lpicker = 1
     qsloutput = (f"{qlist[qpicker]} {slist[spicker]} {llist[lpicker]}")
@@ -57,51 +57,55 @@ qpicklist = [-1]
 spicklist = [-1]
 lpicklist = [-1]
 if str(args.outputs).isnumeric() == True:
-    try:
-        os.makedirs((currentdir) + '/Output/')
-    except OSError as exc:  # handles the error if the directory already exists
-        if exc.errno != errno.EEXIST:
-            raise
-        pass
-    today = str(date.today())
-    path = currentdir + "/Output/" + (filename)  # Creates the root of the output directory path
-    increment = 0
-    while os.path.exists(f"{path} {today} {str(increment)}.txt"):
-        increment += 1
-    path = (f"{path} {today} {str(increment)}")  # Adds today's date and an increment number to the output directory path
-    f = open((path) + '.txt', "w")
-    f.write(f"QSL output of {args.outputs} QSLs on {today}") # writes header
-    f.write('\n\n--------\n\n')
-    for x in range(int(args.outputs)): # creates entries
-        outputlist = QSLer(qpicklist,spicklist,lpicklist)
-        qsloutput = outputlist[0]
-        qpicklist.append(outputlist[1])
-        spicklist.append(outputlist[2])
-        lpicklist.append(outputlist[3])
-        f.write(qsloutput + '\n')
-    del qpicklist[0] # these lines clean up the picklists
-    del spicklist[0]
-    del lpicklist[0]
-    f.write('\n\n--------\n\n')
-    f.write('LOG\n\n')
-    for x in range (len(qpicklist)):
-        if len(str(qpicklist[x])) == 1:
-            f.write('0')
-        f.write(str(qpicklist[x]))
-        f.write(' ')
-    f.write('\n')
-    for x in range (len(spicklist)):
-        if len(str(spicklist[x])) == 1:
-            f.write('0')
-        f.write(str(spicklist[x]))
-        f.write(' ')
-    f.write('\n')
-    for x in range (len(lpicklist)):
-        if len(str(lpicklist[x])) == 1:
-            f.write('0')
-        f.write(str(lpicklist[x]))
-        f.write(' ')
-    f.write('\n')
-    f.close()
+    if int(args.outputs) < 10000 and int(args.outputs) >= 1:
+        try:
+            os.makedirs((currentdir) + '/Output/')
+        except OSError as exc:  # handles the error if the directory already exists
+            if exc.errno != errno.EEXIST:
+                raise
+            pass
+        today = str(date.today())
+        path = currentdir + "/Output/" + (filename)  # Creates the root of the output directory path
+        increment = 0
+        while os.path.exists(f"{path} {today} {str(increment)}.txt"):
+            increment += 1
+        path = (f"{path} {today} {str(increment)}")  # Adds today's date and an increment number to the output directory path
+        f = open((path) + '.txt', "w")
+        f.write(f"QSL output of {args.outputs} QSLs on {today}\n\n--------\n\n") # writes header
+        for x in range(int(args.outputs)): # creates entries
+            outputlist = QSLer(qpicklist,spicklist,lpicklist)
+            qsloutput = outputlist[0]
+            qpicklist.append(outputlist[1])
+            spicklist.append(outputlist[2])
+            lpicklist.append(outputlist[3])
+            f.write(qsloutput + '\n')
+        del qpicklist[0] # these lines clean up the picklists
+        del spicklist[0]
+        del lpicklist[0]
+        f.write('\n\n--------\n\nLOG\n\nQ PICKS\n') # the rest of this 'if' writes the log of pick numbers
+        for x in range (len(qpicklist)):
+            if x % 30 == 0 and x != 0: # organises numbers into blocks
+                f.write ('\n')
+            if len(str(qpicklist[x])) == 1: # adds 0's to 1 digit numbers
+                f.write('0')
+            f.write(str(qpicklist[x])+' ')
+        f.write('\n\nS PICKS\n')
+        for x in range (len(spicklist)):
+            if x % 30 == 0 and x != 0:
+                f.write ('\n')
+            if len(str(spicklist[x])) == 1:
+                f.write('0')
+            f.write(str(spicklist[x])+' ')
+        f.write('\n\nL PICKS\n')
+        for x in range (len(lpicklist)):
+            if x % 30 == 0 and x != 0:
+                f.write ('\n')
+            if len(str(lpicklist[x])) == 1:
+                f.write('0')
+            f.write(str(lpicklist[x])+' ')
+        f.write('\n')
+        f.close()
+    else:
+        print ('ERROR: argument must be between 1 and 9999.')
 else:
-    print ('ERROR: Must specify a number of QSLs to output. e.g. "QSL.py 32"\n')
+    print ('ERROR: Must specify a number of QSLs to output. e.g. "QSL.py 32".')
--- README.md
+++ README.md
@@ -1,6 +1,23 @@
 # QSL
-<<<<<<< HEAD
-Creates random phrases beginning with 'Q', 'S' and 'L'
-=======
-Creates random phrases beginning with 'Q', 'S' and 'L'
->>>>>>> 8f2434cf71f40e6883dafae5b021c07e759ec722
+Creates random three word phrases beginning with 'Q', 'S' and 'L', for example:
+
+*Quirky Slothful Lapdance   
+Queenly Scrofulous Lynchpin   
+Quarantined Starfish Lazyness   
+Quintessential Sexual Lordship    
+Quiet Scarf Lactators   
+Qualitative Smurf Lips   
+Quarantined Satisfaction Listicle*   
+
+and stores them in a txt file in the **Output** directory.
+
+###Instructions:
+
+To use this script, run it from the command line as a normal python script with
+an argument consisting of a whole number between 1 and 9999, for example:
+
+**\>QSL.py 40**   
+**\>QSL.py 256**   
+**\>QSL.py 93**
+
+etc.
